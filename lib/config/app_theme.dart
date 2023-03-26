7ee3aa3437f1b1e46a1c751d8d4d15064dca4808
danielElcorp <dannydix555@gmail.com>
2023-03-26 07:42:40 +0100

    adds config/appTheme

---
+++ lib/config/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+final appTheme = ThemeData(
+    primarySwatch: Colors.green,
+    appBarTheme:
+        const AppBarTheme(backgroundColor: Colors.transparent, elevation: 0, centerTitle: true),
+    textTheme:
+        const TextTheme(headlineMedium: TextStyle(fontFamily: "Pangram")));
